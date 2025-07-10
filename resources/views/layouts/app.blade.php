6c5beaa34e2e31c5a9ee380b3659e57a14199e30
Sean <sean.mic75@gmail.com>
2025-07-09 22:27:52 -0400

    modify chairperson dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,39 +7,37 @@
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-4">
-        <a class="navbar-brand" href="#">CapTrack</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+    <a class="navbar-brand" href="#">CapTrack</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
-        <div class="collapse navbar-collapse justify-content-end" id="navbarContent">
-            <ul class="navbar-nav align-items-center">
-                @if (Auth::check() && Auth::user()->role === 'chairperson')
-                    <li class="nav-item">
-                        <a class="nav-link" href="/chairperson/offerings">Offerings</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/chairperson/teachers">Teachers</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/chairperson/schedules">Schedules</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/chairperson/assign">Assign Offerings</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/manage-roles">Manage Roles</a>
-                    </li>
-                @endif
-                <li class="nav-item ms-3">
-                    <form method="POST" action="{{ route('logout') }}">
-                        @csrf
-                        <button class="btn btn-outline-light btn-sm" type="submit">Logout</button>
-                    </form>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarContent">
+        <ul class="navbar-nav align-items-center">
+            @if (Auth::check() && Auth::user()->role === 'chairperson')
+                <li class="nav-item">
+                    <a class="nav-link" href="/chairperson/offerings">Offerings</a>
                 </li>
-            </ul>
-        </div>
-    </nav>
+                <li class="nav-item">
+                    <a class="nav-link" href="/chairperson/teachers">Teachers</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/chairperson/schedules">Schedules</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/manage-roles">Manage Roles</a>
+                </li>
+            @endif
+            <li class="nav-item ms-3">
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <button class="btn btn-outline-light btn-sm" type="submit">Logout</button>
+                </form>
+            </li>
+        </ul>
+    </div>
+</nav>
+
 
     <main class="py-4 container">
         @yield('content')
