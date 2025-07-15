ac3d96f2880b33f73a4b8cce272df16b8a5852c5
Sean <sean.mic75@gmail.com>
2025-07-15 01:47:02 -0400

    modify chairperson dashboard

--- resources/views/chairperson/dashboard.blade.php
+++ resources/views/chairperson/dashboard.blade.php
@@ -5,53 +5,53 @@
         <h1 class="mb-4">Welcome, {{ auth()->user()->name }}</h1>
         <p class="lead">Chairperson Dashboard</p>
 
-        <div class="row mt-5">
+        <div class="row mt-5 justify-content-center">
             {{-- Manage Offerings --}}
-            <div class="col-md-3">
-                <div class="card shadow">
-                    <div class="card-body">
+            <div class="col-md-2 mb-4">
+                <div class="card shadow h-100">
+                    <div class="card-body d-flex flex-column justify-content-between">
                         <h5 class="card-title">Manage Offerings</h5>
-                        <a href="{{ route('chairperson.offerings.index') }}" class="btn btn-primary">Go</a>
+                        <a href="{{ route('chairperson.offerings.index') }}" class="btn btn-primary mt-3">Go</a>
                     </div>
                 </div>
             </div>
 
             {{-- View Teachers --}}
-            <div class="col-md-3">
-                <div class="card shadow">
-                    <div class="card-body">
+            <div class="col-md-2 mb-4">
+                <div class="card shadow h-100">
+                    <div class="card-body d-flex flex-column justify-content-between">
                         <h5 class="card-title">View Teachers</h5>
-                        <a href="{{ route('chairperson.teachers.index') }}" class="btn btn-primary">Go</a>
+                        <a href="{{ route('chairperson.teachers.index') }}" class="btn btn-primary mt-3">Go</a>
                     </div>
                 </div>
             </div>
 
             {{-- View Schedules --}}
-            <div class="col-md-3">
-                <div class="card shadow">
-                    <div class="card-body">
+            <div class="col-md-2 mb-4">
+                <div class="card shadow h-100">
+                    <div class="card-body d-flex flex-column justify-content-between">
                         <h5 class="card-title">View Schedules</h5>
-                        <a href="{{ route('chairperson.schedules') }}" class="btn btn-primary">Go</a>
+                        <a href="{{ route('chairperson.schedules') }}" class="btn btn-primary mt-3">Go</a>
                     </div>
                 </div>
             </div>
 
             {{-- Import Students --}}
-            <div class="col-md-3">
-                <div class="card shadow">
-                    <div class="card-body">
+            <div class="col-md-2 mb-4">
+                <div class="card shadow h-100">
+                    <div class="card-body d-flex flex-column justify-content-between">
                         <h5 class="card-title">Import Students</h5>
-                        <a href="{{ route('chairperson.upload-form') }}" class="btn btn-primary">Import</a>
+                        <a href="{{ route('chairperson.upload-form') }}" class="btn btn-primary mt-3">Import</a>
                     </div>
                 </div>
             </div>
 
             {{-- Manage Roles --}}
-            <div class="col-md-3 mt-4">
-                <div class="card shadow">
-                    <div class="card-body">
+            <div class="col-md-2 mb-4">
+                <div class="card shadow h-100">
+                    <div class="card-body d-flex flex-column justify-content-between">
                         <h5 class="card-title">Manage Roles</h5>
-                        <a href="{{ url('/chairperson/manage-roles') }}" class="btn btn-primary">Manage</a>
+                        <a href="{{ url('/chairperson/manage-roles') }}" class="btn btn-primary mt-3">Manage</a>
                     </div>
                 </div>
             </div>
